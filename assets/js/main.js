8138c737b03ff3dc1a9184857deb6182a5d424ac
Vamsi Krishna <vamsitvk225@gmail.com>
2026-05-13 17:17:51 +0530

    91cent

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,4 +110,23 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
+
+    // 6. Password Visibility Toggle
+    const togglePasswordButtons = document.querySelectorAll('.password-toggle');
+    togglePasswordButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const input = this.parentElement.querySelector('input');
+            const icon = this.querySelector('i');
+            
+            if (input.type === 'password') {
+                input.type = 'text';
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                input.type = 'password';
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
+        });
+    });
 });
